008ab91fd162c4fb0e3bc99fd34f3471740a5901
cisar2218 <cisar2218@gmail.com>
2025-12-19 22:05:02 +0100

    IInteractable Interface added

---
+++ Time To Sleep/Assets/Scripts/NPC.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour, IInteractable
+{
+
+    void IInteractable.Interact(GameObject interactor)
+    {
+        // Implementation of interaction logic with the NPC
+    }
+}
